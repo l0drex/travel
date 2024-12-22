e3391faf3cce06dc187750128f5a446fdbdcfdac
l0drex <l0drex@pm.me>
2024-12-22 17:44:18 +0100

    Put earth in background properly

--- src/components/travel/EarthOverview.vue
+++ src/components/travel/EarthOverview.vue
@@ -16,38 +16,10 @@ const {journeys} = defineProps<{
 
 <template>
   <div id="container">
-    <TresCanvas v-if="enableAnimatedEarth" id="canvas">
+    <TresCanvas v-if="enableAnimatedEarth" id="canvas" window-size>
       <EarthContent :journeys="journeys"/>
     </TresCanvas>
 
     <slot v-else/>
   </div>
 </template>
-
-<style scoped>
-#container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -100;
-  
-  width: 100%;
-  height: 100%;
-}
-
-#canvas {
-  height: 100%;
-}
-
-@media (min-height: 850px) {
-  #container {
-    height: 600px;
-  }
-}
-
-@media (min-height: 1000px) {
-  #container {
-    height: 800px;
-  }
-}
-</style>
