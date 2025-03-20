21276f6ae3669b86fb76f631ba1a36e67ae7915c
l0drex <l0drex@pm.me>
2025-03-20 17:13:29 +0100

    Link to heading from map

--- src/components/track/MapView.vue
+++ src/components/track/MapView.vue
@@ -7,6 +7,7 @@ import { computed } from "vue";
 import LeafletMap from "src/leaflet/components/LeafletMap.vue";
 import TileLayer from "src/leaflet/components/TileLayer.vue";
 import GeoJsonLayer from "src/leaflet/components/GeoJsonLayer.vue";
+import Slugger from "github-slugger";
 
 const { geoJson } = defineProps<{
   geoJson: GeoJSON;
@@ -20,35 +21,47 @@ const trackColors = [
 ];
 const currentUrlTitle = useUrlTitle();
 
-const geoJsonOptions = computed<L.GeoJSONOptions>(() => {
+const styleFunction = computed<L.StyleFunction>(() => {
   let title = currentUrlTitle.value;
   let index = 0;
 
-  return {
-    style: (feature) => {
-      // apply differing styles by default
-      let color = trackColors[index % 2];
+  return (feature) => {
+    // apply differing styles by default
+    let color = trackColors[index % 2];
 
-      // if a title is selected, highlight current track instead
-      // NOTE this will turn all tracks to the same color if no track has the corresponding title
-      if (title != null) {
-        const currentFeatureTitle = feature?.properties?.name;
+    // if a title is selected, highlight current track instead
+    // NOTE this will turn all tracks to the same color if no track has the corresponding title
+    if (title != null) {
+      const currentFeatureTitle = feature?.properties?.name;
 
-        if (currentFeatureTitle == title) {
-          color = trackColors[0];
-        } else {
-          color = trackColors[1];
-        }
+      if (currentFeatureTitle == title) {
+        color = trackColors[0];
+      } else {
+        color = trackColors[1];
       }
+    }
 
-      index++;
-      return {
-        color: color.value,
-        weight: 5,
-      };
-    },
+    index++;
+    return {
+      color: color.value,
+      weight: 5,
+    };
   };
 });
+
+// astro uses rehype-slug, which uses github-slugger, so we use this here to convert the heading to the id
+// this might break in the future, since we are using the implementation 2 dependencies down...
+const slugger = new Slugger();
+
+const geoJsonOptions: L.GeoJSONOptions = {
+  style: styleFunction.value,
+  onEachFeature: (feature, layer) => {
+    layer.bindPopup(
+      // link to heading in current markdown post with id of feature as name
+      `<a href="#${slugger.slug(feature.properties["name"])}">${feature.properties["name"]}</a>`,
+    );
+  },
+};
 
 const tileOptions: L.TileLayerOptions = {
   attribution: "<a href='https://openstreetmap.de'>&copy; OpenStreetMap DE</a>",
