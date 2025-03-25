86da488a357e5278ed4f1aec37bc34ee55949a1f
Lorenz Hoffmann <l0drex@pm.me>
2025-03-25 22:36:46 +0100

    Load threejs only on demand

--- src/components/earth/EarthFallback.vue
+++ src/components/earth/EarthFallback.vue
@@ -1,20 +1,5 @@
 <script setup lang="ts">
-import WebGL from "three/examples/jsm/capabilities/WebGL";
-import type { Journey } from "@utils/types.ts";
-import { usePreferredReducedMotion } from "@vueuse/core";
-import { getColorPropertyString } from "@utils/color.ts";
-import { defineAsyncComponent } from "vue";
-
-const TresCanvas = defineAsyncComponent(() => import("@tresjs/core").then(t => t.TresCanvas));
-const EarthContent = defineAsyncComponent(() => import("./EarthContent.vue"));
-
-const reducedMotion = usePreferredReducedMotion();
-const enableAnimatedEarth: boolean =
-  WebGL.isWebGL2Available() && reducedMotion.value != "reduce";
-
-const { journeys } = defineProps<{
-  journeys: Journey[];
-}>();
+import { getColorPropertyString } from "@utils/color.js";
 
 // tailwind colors
 const bg = getColorPropertyString("bg");
@@ -24,12 +9,7 @@ const bg2Dark = getColorPropertyString("bg-2-dark");
 </script>
 
 <template>
-  <TresCanvas v-if="enableAnimatedEarth" id="canvas" window-size>
-    <EarthContent :journeys="journeys" />
-  </TresCanvas>
-
   <svg
-    v-else
     viewBox="0 5 100 90"
     xmlns="http://www.w3.org/2000/svg"
     class="h-screen w-screen"
