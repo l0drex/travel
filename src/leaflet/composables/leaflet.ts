486b1c6dd895b96751b969b401ec0df5389549f9
l0drex <l0drex@pm.me>
2025-03-29 18:06:55 +0100

    Fix missing marker icons

--- src/leaflet/composables/leaflet.ts
+++ src/leaflet/composables/leaflet.ts
@@ -1,5 +1,14 @@
-import { reactive, ref, useTemplateRef, watch } from "vue";
+import { ref, watch } from "vue";
 import L from "leaflet";
+import markerIconUrl from "leaflet/dist/images/marker-icon.png";
+import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+// fix no icons in build
+L.Icon.Default.prototype.options.iconUrl = markerIconUrl.src;
+L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl.src;
+L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl.src;
+L.Icon.Default.imagePath = "";
 
 const map = ref<L.Map>();
 
