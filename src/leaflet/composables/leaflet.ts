300eea7a96fa37978083d5574ad488b3f9292ade
l0drex <l0drex@pm.me>
2025-03-29 18:42:03 +0100

    Use custom icon

--- src/leaflet/composables/leaflet.ts
+++ src/leaflet/composables/leaflet.ts
@@ -1,13 +1,16 @@
 import { ref, watch } from "vue";
 import L from "leaflet";
-import markerIconUrl from "leaflet/dist/images/marker-icon.png";
-import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import markerIconUrl from "@assets/marker.svg";
 import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
 
 // fix no icons in build
 L.Icon.Default.prototype.options.iconUrl = markerIconUrl.src;
-L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl.src;
+L.Icon.Default.prototype.options.iconSize = [48, 48];
+L.Icon.Default.prototype.options.iconAnchor = [48 / 2, 48 - 4];
+L.Icon.Default.prototype.options.iconRetinaUrl = markerIconUrl.src;
 L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl.src;
+L.Icon.Default.prototype.options.shadowSize = [48, 48];
+L.Icon.Default.prototype.options.shadowAnchor = [48 / 2 - 6, 48];
 L.Icon.Default.imagePath = "";
 
 const map = ref<L.Map>();
