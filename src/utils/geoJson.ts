143a7c63b989c7d954bab9af7c0641bfea5ad535
l0drex <l0drex@pm.me>
2025-05-05 12:18:24 +0200

    Calculate stats in one point

--- src/utils/geoJson.ts
+++ src/utils/geoJson.ts
@@ -1,6 +1,7 @@
 import { MathUtils, Vector3 } from "three";
 import type { GeoJSON, Position } from "geojson";
-import { coordAll, flatten } from "@turf/turf";
+import { coordAll, coordEach, distance, flatten, length } from "@turf/turf";
+import { statsPrototypes } from "@utils/types.ts";
 
 export function getFeatureByName(
   name: string,
@@ -55,4 +56,57 @@ export function positionAtCoordinate(
 
   vec.setFromSphericalCoords(r, phi, theta);
   return vec;
+}
+
+type StatsType = { [K in keyof typeof statsPrototypes]?: number };
+export function calculateStats(stats: StatsType, geoJson: GeoJSON): StatsType {
+  // trip length
+  stats["totalDistance"] = length(geoJson as any);
+
+  // calculate elevation gained and lost
+  // this is the height in meters above sea level,
+  // not the distance where elevation was gained or lost
+  const minDistance = 0.5; // kilometers
+  let elevationUp = 0;
+  let elevationDown = 0;
+  let lastCoordinate: number[];
+  coordEach(geoJson, (currentCoord) => {
+    if (!lastCoordinate) {
+      lastCoordinate = currentCoord;
+      return;
+    }
+
+    let elevationDelta = currentCoord[2] - lastCoordinate[2];
+    if (distance(lastCoordinate, currentCoord) < minDistance) {
+      return;
+    }
+
+    if (elevationDelta > 0) {
+      elevationUp += elevationDelta;
+    } else if (elevationDelta < 0) {
+      elevationDown -= elevationDelta;
+    }
+
+    lastCoordinate = currentCoord;
+  });
+  stats["elevationUp"] = elevationUp;
+  stats["elevationDown"] = elevationDown;
+
+  // calculate kilometers and time per day
+  if ("days" in stats) {
+    if ("totalDistance" in stats) {
+      stats["kilometerPerDay"] = stats.totalDistance / stats.days!;
+    }
+
+    if ("totalTime" in stats) {
+      stats["timePerDay"] = stats.totalTime! / stats.days!;
+    }
+  }
+
+  // calculate average speed
+  if ("totalDistance" in stats && "totalTime" in stats) {
+    stats["averageSpeed"] = stats["totalDistance"]! / stats["totalTime"]!;
+  }
+
+  return stats;
 }
