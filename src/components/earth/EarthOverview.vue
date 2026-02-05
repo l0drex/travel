893d9b86173bf536d4c2a3d80302e4f7c9e35395
l0drex <l0drex@pm.me>
2026-02-05 15:52:35 +0100

    Actually use fallback component

--- src/components/earth/EarthOverview.vue
+++ src/components/earth/EarthOverview.vue
@@ -4,6 +4,7 @@ import type { Journey } from "@utils/types.ts";
 import { usePreferredReducedMotion } from "@vueuse/core";
 import { getColorPropertyString } from "@utils/color.ts";
 import { defineAsyncComponent } from "vue";
+import EarthFallback from "@components/earth/EarthFallback.vue";
 
 const TresCanvas = defineAsyncComponent(() =>
   import("@tresjs/core").then((t) => t.TresCanvas),
@@ -17,12 +18,6 @@ const enableAnimatedEarth: boolean =
 const { journeys } = defineProps<{
   journeys: Journey[];
 }>();
-
-// tailwind colors
-const bg = getColorPropertyString("bg");
-const bgDark = getColorPropertyString("bg-dark");
-const bg2 = getColorPropertyString("bg-2");
-const bg2Dark = getColorPropertyString("bg-2-dark");
 </script>
 
 <template>
@@ -36,29 +31,5 @@ const bg2Dark = getColorPropertyString("bg-2-dark");
     <EarthContent :journeys="journeys" />
   </TresCanvas>
 
-  <svg
-    v-else
-    class="h-screen w-screen"
-    viewBox="0 5 100 90"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <defs>
-      <linearGradient id="earthGradient">
-        <stop :stop-color="bg2" offset="0%"></stop>
-        <stop :stop-color="bg" offset="100%"></stop>
-      </linearGradient>
-
-      <linearGradient id="earthGradientDark">
-        <stop :stop-color="bg2Dark" offset="0%"></stop>
-        <stop :stop-color="bgDark" offset="100%"></stop>
-      </linearGradient>
-    </defs>
-
-    <circle
-      class="fill-[url(#earthGradient)] dark:fill-[url(#earthGradientDark)]"
-      cx="50"
-      cy="50"
-      r="50"
-    ></circle>
-  </svg>
+  <EarthFallback v-else />
 </template>
